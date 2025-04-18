89521431d2f671bd156525c01df6ba803c5dc149
rockybhai09rs <rockybhai09rs@gmail.com>
2025-04-19 01:12:56 +0530

    Create log_toggle.py

---
+++ plugins/log_toggle.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+
+client = MongoClient("your_mongo_url")
+db = client["vj_forward"]
+log_pref = db["log_pref"]
+
+async def is_log_enabled(user_id: int) -> bool:
+    data = log_pref.find_one({"_id": user_id})
+    return data.get("enabled", True) if data else True
+
+async def toggle_log(user_id: int) -> bool:
+    current = await is_log_enabled(user_id)
+    new_status = not current
+    log_pref.update_one({"_id": user_id}, {"$set": {"enabled": new_status}}, upsert=True)
+    return new_status
